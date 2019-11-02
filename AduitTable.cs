df3f0a66d95cc8e4b57a7923cb7c5c2bf544777f
Makayla-Skiff <54339097+Makayla-Skiff@users.noreply.github.com>
2019-11-01 20:43:13 -0500

    Add files via upload

---
+++ AduitTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NavistarWebApp.Data
+{
+    public class AduitTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int AID { get; set; }
+
+        public string SLT { get; set; }
+
+
+        [Display(Name = "Approved Positions")]
+        [ForeignKey("Approved")]
+        public Headcount Appr { get; set; }
+
+        [Display(Name = "Employee Count")]
+        public int ECount { get; set; }
+
+        [Display(Name = "Contractor Count")]
+        public int CCount { get; set; }
+
+        [Display(Name = "Open Position Count")]
+        public int OCount { get; set; }
+
+        [Display(Name = "Time Stamp")]
+        public DateTime DTStamp { get; set; }
+
+        [Display(Name = "Update or Submit")]
+        public string UorS { get; set; }
+
+    }
+}
---
+++ ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NavistarWebApp.Data;
+
+namespace NavistarWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NavistarWebApp.Data.DepartmentPage> DepartmentPage { get; set; }
+        public DbSet<NavistarWebApp.Data.AttritionPage> AttritionPage { get; set; }
+        public DbSet<NavistarWebApp.Data.AdminPage> AdminPage { get; set; }
+        public DbSet<NavistarWebApp.Data.SLTPage> SLTPage { get; set; }
+        public DbSet<NavistarWebApp.Data.AduitTable> AduitTable { get; set; }
+        public DbSet<NavistarWebApp.Data.Employees> Employees { get; set; }
+    }
+}
